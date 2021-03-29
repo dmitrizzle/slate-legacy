e4c7ac421683c4442a04722c56a813d49b93ef3f
dmitrizzle <8587882+dmitrizzle@users.noreply.github.com>
2021-03-28 18:06:05 -0700

    Upgrade package and modify deployment strategy

---
+++ packages/slate-react/slate-legacy-postpublish.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const { exec } = require('child_process')
+
+console.log('> This will restore `package.json` file for local development.')
+
+fs.readFile('./package.json', 'utf8', (error, data) => {
+  if (!error) {
+    // read package contents
+    let package = {}
+    try {
+      package = JSON.parse(data)
+    } catch (error) {
+      throw console.log('> Error: could not parse `package.json`')
+    }
+
+    // Change package name for publication.
+    package.name = 'slate-react'
+
+    // restore package
+    try {
+      fs.writeFile('./package.json', JSON.stringify(package), 'utf8', error => {
+        if (error)
+          throw '> Error: could not create a backup copy of `package.json` file.'
+      })
+    } catch (error) {
+      throw error
+    }
+    console.log('> Successfully saved modified `package.json` file.')
+    return
+  }
+  throw error
+})
---
+++ packages/slate-react/slate-legacy-prepublish.js
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const { exec } = require('child_process')
+
+console.log(
+  '> This will prepare a modified copy of package.json to publish `slate-react-legacy` on npm.'
+)
+
+fs.readFile('./package.json', 'utf8', (error, data) => {
+  if (!error) {
+    // create backup
+
+    // read package contents
+    let package = {}
+    try {
+      package = JSON.parse(data)
+    } catch (error) {
+      throw console.log('> Error: could not parse `package.json`')
+    }
+
+    // Change package name for publication.
+    package.name = 'slate-react-legacy'
+
+    try {
+      fs.writeFile('./package.json', JSON.stringify(package), 'utf8', error => {
+        if (error)
+          throw '> Error: could not create a backup copy of `package.json` file.'
+      })
+    } catch (error) {
+      throw error
+    }
+    console.log('> Successfully saved modified `package.json` file.')
+    return
+  }
+  throw error
+})
